54547b577820bb8a323dd78d1fa4473da3f90552
Martyn Janes <martyn@obany.com>
2020-12-10 10:52:13 +0100

    Repo relocation

--- src/index.node.ts
+++
@@ -1,8 +0,0 @@
-import fetch from "node-fetch";
-
-if (!globalThis.fetch) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    globalThis.fetch = fetch as any;
-}
-
-export * from "./index";
--- src/models/ledgerInclusionState.ts
+++
@@ -1,4 +0,0 @@
-/**
- * The different states of ledger inclusion.
- */
-export type LedgerInclusionState = "noTransaction" | "included" | "conflicting";
